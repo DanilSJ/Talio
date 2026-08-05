33e4d28f4a8d469660cdb6f90d19fd95c75cba77
DanilSJ <danilchagarnoy@mail.ru>
2026-08-05 23:47:41 +0300

    create models

---
+++ core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    "User",
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+]
+
+from .user import User
+from .base import Base
+from .db_helper import DatabaseHelper, db_helper
---
+++ core/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, BigInteger, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime
+from datetime import datetime
+
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String, nullable=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger)
+
+    admin: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    premium: Mapped[bool] = mapped_column(Boolean, default=False)
+    buy_premium: Mapped[datetime] = mapped_column(DateTime(timezone=True))
