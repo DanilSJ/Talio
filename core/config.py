5357456c517d5989b2d04fbe474bcd020b7c7534
DanilSJ <danilchagarnoy@mail.ru>
2026-08-05 23:47:30 +0300

    add new field db_url, DB_ECHO, DB_POOL_NULL

--- core/config.py
+++ core/config.py
@@ -1,13 +1,20 @@
+import pathlib
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+
 
 class Settings(BaseSettings):
     TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN")
     PROXY: str = os.getenv("PROXY")
+
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    DB_ECHO: bool = os.getenv("DB_ECHO", "False") == "False"
+    DB_POOL_NULL: bool = os.getenv("DB_POOL_NULL", "False") == "True"
 
 
 settings = Settings()
