a730e623c3c8dcf9d396f3613ebda2db261e13aa
DanilSJ <danilchagarnoy@mail.ru>
2026-08-05 23:58:10 +0300

    add func create_user

--- app/start/handler.py
+++ app/start/handler.py
@@ -1,12 +1,16 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-
+from app.start.crud import create_user
 from app.start.keyboard import main_menu
+from core.models import db_helper
 
 router = Router()
 
 
 @router.message(Command("start"))
 async def start(message: Message):
-    await message.answer("Hello world", reply_markup=main_menu())
+    async with db_helper.scoped_session_dependency() as session:
+        await create_user(session, message.from_user.id, message.from_user.username)
+
+    return await message.answer("Hello world", reply_markup=main_menu())
