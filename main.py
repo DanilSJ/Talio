8f0ea3d4e8b0400bb9e18de026b1b1f4bc14e431
DanilSJ <danilchagarnoy@mail.ru>
2026-08-05 22:50:19 +0300

    create start bot

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from talio!")
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+from core.config import settings
+
+session = AiohttpSession(proxy=settings.PROXY)
+bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, session=session)
+
+dp = Dispatcher()
+
+
+async def main():
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
