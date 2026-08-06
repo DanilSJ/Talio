a1f4e824bbcf31f1a693de2dade8777c0ebfca2d
DanilSJ <danilchagarnoy@mail.ru>
2026-08-06 13:18:24 +0300

    create func add_referrer

--- app/start/crud.py
+++ app/start/crud.py
@@ -1,6 +1,6 @@
 from typing import Optional
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import User
 
 
@@ -26,6 +26,34 @@ async def create_user(
     )
 
     session.add(user)
+    await session.commit()
+    await session.refresh(user)
+
+    return user
+
+
+async def add_referrer(
+    session: AsyncSession,
+    telegram_id: int,
+    referrer_id: int,
+) -> Optional[User]:
+    stmt = select(User).where(User.telegram_id == referrer_id)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    if not user:
+        return None
+
+    stmt_referrer = select(User).where(User.telegram_id == telegram_id)
+    result_referrer = await session.execute(stmt_referrer)
+    referrer = result_referrer.scalar_one_or_none()
+
+    if not referrer:
+        return None
+
+    user.referrer_id = referrer.id
+    user.referrer_is_active = True
+
     await session.commit()
     await session.refresh(user)
 
