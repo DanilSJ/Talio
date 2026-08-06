91114ea7626745a67a00d5106ba74683a2180939
DanilSJ <danilchagarnoy@mail.ru>
2026-08-06 14:37:34 +0300

    create update_premium

---
+++ app/payment/crud.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import Optional
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from core.models import User
+
+
+async def update_premium(
+    session: AsyncSession,
+    telegram_id: int,
+) -> Optional[User]:
+    stmt = select(User).where(User.telegram_id == telegram_id)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    if not user:
+        return None
+
+    user.premium = True
+    user.buy_premium = datetime.now()
+
+    await session.commit()
+    await session.refresh(user)
+
+    return user
