81c03a586e334efc911d74880a61ee29319e7aea
DanilSJ <danilchagarnoy@mail.ru>
2026-08-06 22:07:01 +0300

    create func del_referrer

--- app/payment/crud.py
+++ app/payment/crud.py
@@ -23,3 +23,21 @@ async def update_premium(
     await session.refresh(user)
 
     return user
+
+async def del_referrer(
+    session: AsyncSession,
+    telegram_id: int,
+) -> Optional[User]:
+    stmt = select(User).where(User.telegram_id == telegram_id)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    if not user:
+        return None
+
+    user.referrer_is_active = False
+
+    await session.commit()
+    await session.refresh(user)
+
+    return user
