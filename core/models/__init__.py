cbfb50c9e8e145393e87e63f15df7e233ae15d33
DanilSJ <danilchagarnoy@mail.ru>
2026-08-06 13:05:33 +0300

    add model ai, message

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,10 +1,14 @@
 __all__ = [
+    "AI",
+    "Message",
     "User",
     "Base",
     "DatabaseHelper",
     "db_helper",
 ]
 
+from .ai import AI
+from .message import Message
 from .user import User
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
