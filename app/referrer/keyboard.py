4467ef60096cc4c99971d3eecf8294bbe0b15003
DanilSJ <danilchagarnoy@mail.ru>
2026-08-07 10:20:09 +0300

    create referrer_keyboard

---
+++ app/referrer/keyboard.py
@@ -0,0 +1,29 @@
+from aiogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CopyTextButton,
+    SwitchInlineQueryChosenChat,
+)
+
+
+def referrer_keyboard(url: str):
+    builder = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Скопировать ссылку",
+                    copy_text=CopyTextButton(text=url),
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="Отправить друзьям",
+                    switch_inline_query_chosen_chat=SwitchInlineQueryChosenChat(
+                        query=url
+                    ),
+                ),
+            ],
+        ]
+    )
+
+    return builder
