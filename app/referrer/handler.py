9f391942a45105bb09c4edfceaf6318368e20f3e
DanilSJ <danilchagarnoy@mail.ru>
2026-08-07 10:52:44 +0300

    set parse mode on html

--- app/referrer/handler.py
+++ app/referrer/handler.py
@@ -18,10 +18,13 @@ async def referrer(message: Message):
         )
         url = f"https://t.me/{settings.BOT_USERNAME}?start={user.telegram_id}"
         return await message.answer(
-            f"""*Пригласите друзей*
-            Ваша персональная реферальная ссылка: {url}
-Вы уже пригласили {await get_referrals_count(session, message.from_user.id)} чел
+            f"""<b>Пригласите друзей</b>
+            
+Ваша персональная реферальная ссылка: {url}
+Вы уже пригласили {await get_referrals_count(session, message.from_user.id)} человек.
+
 За одного приглашенного вы получите скидку 10% на Premium (скидка не суммируется за приглашенных людей)
 Отправьте ссылку другу или нажмите кнопку ниж для быстрой отправки:""",
             reply_markup=referrer_keyboard(url),
+            parse_mode="HTML",
         )
