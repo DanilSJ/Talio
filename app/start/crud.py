a93b1d610b0ff53af826292059740484dcc1a871
DanilSJ <danilchagarnoy@mail.ru>
2026-08-07 10:21:28 +0300

    fix bug message

--- app/start/crud.py
+++ app/start/crud.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
 from core.models import User
 
 
@@ -9,7 +10,7 @@ async def create_user(
     telegram_id: int,
     username: Optional[str] = None,
 ) -> User:
-    stmt = select(User).where(User.telegram_id == telegram_id)
+    stmt = select(User).where(User.telegram_id == telegram_id).options(selectinload(User.referred_users))
     result = await session.execute(stmt)
     existing_user = result.scalar_one_or_none()
 
@@ -32,29 +33,42 @@ async def create_user(
     return user
 
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Optional
+
+
 async def add_referrer(
     session: AsyncSession,
-    telegram_id: int,
-    referrer_id: int,
+    invited_telegram_id: int,
+    referrer_telegram_id: int,
 ) -> Optional[User]:
-    stmt = select(User).where(User.telegram_id == referrer_id)
-    result = await session.execute(stmt)
-    user = result.scalar_one_or_none()
-
-    if not user:
-        return None
-
-    stmt_referrer = select(User).where(User.telegram_id == telegram_id)
+    stmt_referrer = select(User).where(User.telegram_id == referrer_telegram_id)
     result_referrer = await session.execute(stmt_referrer)
     referrer = result_referrer.scalar_one_or_none()
 
     if not referrer:
         return None
 
-    user.referrer_id = referrer.id
-    user.referrer_is_active = True
+    stmt_invited = select(User).where(User.telegram_id == invited_telegram_id)
+    result_invited = await session.execute(stmt_invited)
+    invited_user = result_invited.scalar_one_or_none()
+
+    if not invited_user:
+        return None
+
+    if invited_user.referrer_id is not None:
+        return None
+
+    if invited_telegram_id == referrer_telegram_id:
+        return None
+
+    invited_user.referrer_id = referrer.id
+    invited_user.referrer_is_active = True
 
     await session.commit()
-    await session.refresh(user)
 
-    return user
+    await session.refresh(invited_user)
+    await session.refresh(referrer)
+
+    return invited_user
