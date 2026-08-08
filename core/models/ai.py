2e68ca2aff1a9fd8293dda3d19296f9d11e33dc3
DanilSJ <danilchagarnoy@mail.ru>
2026-08-08 13:55:01 +0300

    add field qwen_use

--- core/models/ai.py
+++ core/models/ai.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Text
+from sqlalchemy import Text, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
 
 class AI(Base):
     system_prompt: Mapped[str] = mapped_column(Text)
+    qwen_use: Mapped[bool] = mapped_column(Boolean, nullable=True, default=False)
